9ed9e2d8df45ddf1faf72913611ca194bed63b27
jonxu88 <jonxu88@gmail.com>
2023-11-25 21:03:14 +0900

    add three_js stuff

--- three_js_test/main.js
+++ three_js_test/main.js
@@ -1,26 +1,31 @@
-import * as THREE from 'three';
-
+// Initialize the scene
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+// Create a perspective camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
+// Create a WebGLRenderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+// Create a cube and add it to the scene
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0xff5733 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-	renderer.render( scene, camera );
-}
+// Animation function
+const animate = () => {
+    requestAnimationFrame(animate);
 
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+// Start the animation
 animate();
